4b54cd5c74b4ede8dc9465b1e41bdd3793ff025d
MichaelAChrisco <michaelachrisco@gmail.com>
2014-12-30 15:19:15 -0800

    Driver Cassettes. For testing.

--- lib/gman_client.rb
+++ lib/gman_client.rb
@@ -6,7 +6,6 @@ require 'httparty'
 module GmanClient
   def self.receive_drivers
     request = HTTParty.get('http://localhost:3000/api/v1/drivers.json')
-    # request = HTTParty.get('http://example.com//api/v1/drivers.json')
     request.parsed_response.map(&:to_snake_keys)
   end
 end
