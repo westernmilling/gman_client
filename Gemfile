fcc642923a50ad5e0c1458043a15817f17ea140c
MichaelAChrisco <michaelachrisco@gmail.com>
2014-12-30 15:58:47 -0800

    Started on json_request interactor test.

--- Gemfile
+++ Gemfile
@@ -2,5 +2,6 @@ source 'https://rubygems.org'
 
 gem 'plissken', :github => 'futurechimp/plissken'
 gem 'httparty'
+gem 'interactor'
 # Specify your gem's dependencies in gman_client.gemspec
 gemspec
---
+++ spec/lib/interactors/json_request.rb
@@ -0,0 +1,13 @@
+# require 'rails_helper'
+#
+# describe JsonRequest do
+#   describe '#call' do
+#     subject(:context) { interactor }
+#     describe 'context' do
+#       it 'succeeds' do
+#         is_expected.to be_truthy
+#       end
+#     end
+#
+#   end
+# end
