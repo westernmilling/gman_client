6a290a52c0ad197ce40d72e180b4c2618809846f
MichaelAChrisco <michaelachrisco@gmail.com>
2014-12-31 08:53:25 -0800

    Blanket API calls.

--- lib/gman_client.rb
+++ lib/gman_client.rb
@@ -1,11 +1,11 @@
 require 'gman_client/version'
-require 'plissken'
-require 'httparty'
+require 'blanket'
 
 # GmanClient
 module GmanClient
   def self.receive_drivers
-    request = HTTParty.get('http://localhost:3000/api/v1/drivers.json')
-    request.parsed_response.map(&:to_snake_keys)
+    request = Blanket.wrap("http://localhost:3000", :extension => :json)
+    respond = request.api.v1.drivers.get
+    respond.map(&:to_h)
   end
 end
