212cf4153c5a970878c510ac15fdc155b02f4e87
MichaelAChrisco <michaelachrisco@gmail.com>
2015-01-06 17:45:40 -0800

    Driver Commissions API.

--- gman_client.gemspec
+++ gman_client.gemspec
@@ -6,7 +6,7 @@ require 'gman_client/version'
 Gem::Specification.new do |spec|
   spec.name          = 'gman_client'
   spec.version       = GmanClient::VERSION
-  spec.authors       = ['MichaelAChrisco']
+  spec.authors       = ['MichaelAChrisco', 'josephbridgwaterrowe']
   spec.email         = ['michaelachrisco@gmail.com']
   spec.summary       = 'GMAN gem client'
   spec.description   = 'GMAN gem client for Grossman API'
@@ -23,5 +23,6 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency 'bundler', '~> 1.7'
   spec.add_development_dependency 'rake', '~> 10.0'
   spec.add_development_dependency 'rspec'
+  spec.add_development_dependency 'rest-client'
 
 end
--- spec/lib/driver_response_spec.rb
+++ spec/lib/driver_response_spec.rb
@@ -6,29 +6,31 @@ require 'webmock'
 require 'support/vcr'
 
 RSpec.describe GmanClient do
-  describe '.receive_drivers' do
-    VCR.use_cassette('drivers') do
-      response = GmanClient.drivers
-      subject(:driver_response) { response }
-      let(:driver) { driver_response.first }
-
-      it 'empty is not false' do
-        expect(driver_response.empty?).to be_falsey
+  describe '.drivers' do
+    let(:response) do
+      VCR.use_cassette('drivers') do
+        GmanClient.drivers
       end
+    end
+    subject(:client_response) { response }
+    let(:driver) { client_response.first }
 
-      it 'responds with a hash list' do
-        is_expected.to satisfy {
-                           |h| h.is_a?(Array) && h.all? { |e| e.is_a?(Hash) }
-                       }
-      end
-      
-      describe 'first driver' do
-        subject { driver_response.first }
+    it 'client response will not be empty' do
+      expect(client_response.empty?).to be_falsey
+    end
 
-        it { is_expected.to have_key(:driver_id) }
-        it { is_expected.to have_key(:first_name) }
-        it { is_expected.to have_key(:last_name) }
-      end
+    it 'responds with a hash list' do
+      is_expected.to satisfy {
+                         |h| h.is_a?(Array) && h.all? { |e| e.is_a?(Hash) }
+                     }
+    end
+
+    describe 'first driver' do
+      subject { client_response.first }
+
+      it { is_expected.to have_key(:driver_id) }
+      it { is_expected.to have_key(:first_name) }
+      it { is_expected.to have_key(:last_name) }
     end
   end
 end
