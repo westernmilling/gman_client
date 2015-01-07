2c50e9e18bb7b555cef40c35aa3e3c4c5b202ef9
MichaelAChrisco <michaelachrisco@gmail.com>
2015-01-06 17:45:40 -0800

    Driver Commissions API.

--- lib/gman_client.rb
+++ lib/gman_client.rb
@@ -8,4 +8,14 @@ module GmanClient
     respond = request.api.v1.drivers.get
     respond.map(&:to_h)
   end
+  def self.driver_commission_histories
+    request = Blanket.wrap("http://localhost:3000", :extension => :json)
+    respond = request.api.v1.driver_commissions_history.get
+    respond.map(&:to_h)
+  end
+  def self.driver_commission_histories_by_paid_date(paid_date)
+    request = Blanket.wrap("http://localhost:3000", :extension => :json)
+    respond = request.api.v1.driver_commissions_history_by_paid_date.get(params: {:paid_date => paid_date})
+    respond.map(&:to_h)
+  end
 end
---
+++ spec/lib/driver_commission_history_response_spec.rb
@@ -0,0 +1,87 @@
+require 'spec_helper'
+require 'gman_client'
+require 'vcr'
+require 'httparty'
+require 'webmock'
+require 'support/vcr'
+
+RSpec.describe GmanClient do
+  describe '.receive_driver_commission_history_response' do
+    VCR.use_cassette('driver_commission_history') do
+      response = GmanClient.driver_commission_histories
+      subject(:driver_response) { response }
+      let(:driver_commission) { driver_response.first }
+
+      it 'empty is not false' do
+        expect(driver_response.empty?).to be_falsey
+      end
+
+      it 'responds with a hash list' do
+        is_expected.to satisfy {
+                           |h| h.is_a?(Array) && h.all? { |e| e.is_a?(Hash) }
+                       }
+      end
+
+      describe 'first driver' do
+        subject { driver_response.first }
+
+        it { is_expected.to have_key(:driver_id) }
+        it { is_expected.to have_key(:backhauls) }
+        it { is_expected.to have_key(:freight_revenue) }
+        it { is_expected.to have_key(:customer_id) }
+        it { is_expected.to have_key(:delivery_date) }
+        it { is_expected.to have_key(:driver_rate) }
+        it { is_expected.to have_key(:freight_bill_number) }
+        it { is_expected.to have_key(:fuel_surcharge) }
+        it { is_expected.to have_key(:layover) }
+        it { is_expected.to have_key(:movement_type) }
+        it { is_expected.to have_key(:other_hour_dollars) }
+        it { is_expected.to have_key(:order_number_text) }
+        it { is_expected.to have_key(:origin_name) }
+        it { is_expected.to have_key(:paid_date) }
+        it { is_expected.to have_key(:revenue) }
+        it { is_expected.to have_key(:split_rate) }
+        it { is_expected.to have_key(:total_freight_revenue) }
+      end
+    end
+  end
+  describe '.receive_driver_commission_history_response_by_paid_date' do
+    VCR.use_cassette('driver_commission_history_by_paid_date') do
+      response = GmanClient.driver_commission_histories_by_paid_date(Date.new(2012,01,01))
+      subject(:driver_response) { response }
+      let(:driver_commission) { driver_response.first }
+
+      it 'empty is not false' do
+        expect(driver_response.empty?).to be_falsey
+      end
+      it 'responds with a hash list' do
+        is_expected.to satisfy {
+                           |h| h.is_a?(Array) && h.all? { |e| e.is_a?(Hash) }
+                       }
+      end
+
+      describe 'first driver' do
+        subject { driver_response.first }
+
+        it { is_expected.to have_key(:driver_id) }
+        it { is_expected.to have_key(:backhauls) }
+        it { is_expected.to have_key(:freight_revenue) }
+        it { is_expected.to have_key(:customer_id) }
+        it { is_expected.to have_key(:delivery_date) }
+        it { is_expected.to have_key(:driver_rate) }
+        it { is_expected.to have_key(:freight_bill_number) }
+        it { is_expected.to have_key(:fuel_surcharge) }
+        it { is_expected.to have_key(:layover) }
+        it { is_expected.to have_key(:movement_type) }
+        it { is_expected.to have_key(:other_hour_dollars) }
+        it { is_expected.to have_key(:order_number_text) }
+        it { is_expected.to have_key(:origin_name) }
+        it { is_expected.to have_key(:paid_date) }
+        it { is_expected.to have_key(:revenue) }
+        it { is_expected.to have_key(:split_rate) }
+        it { is_expected.to have_key(:total_freight_revenue) }
+        it { is_expected.to have_value(Date.new(2012,01,01).to_s)}
+      end
+    end
+  end
+end
